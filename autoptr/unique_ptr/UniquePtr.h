d9836a064119235c8c741dcec300acb037bba081
jjkingy <1831332707@qq.com>
2025-11-05 00:20:49 +0800

    修改方法

--- autoptr/unique_ptr/UniquePtr.h
+++ autoptr/unique_ptr/UniquePtr.h
@@ -57,7 +57,7 @@ UniquePtr<T>& UniquePtr<T>::operator=(UniquePtr&& other) noexcept {
     if(this != &other) {
         delete ptr_;
         ptr_ = other.ptr_;
-        other.ptr_ == nullptr;
+        other.ptr_ = nullptr;
     }
     return *this;
 }
@@ -69,15 +69,19 @@ void UniquePtr<T>::reset(T* ptr) {
     ptr_ = ptr;
 }
 
+//release只是释放所有权，把管理权转移出去，不销毁对象
+//比如有些接口不适合用智能指针 所以用release把内存转移出去
 template<typename T>
 T* UniquePtr<T>::release() noexcept {
     T* temp = ptr_;
-    delete ptr_;
+    // 只是释放所有权，不删除对象，标识unique_ptr不在管理这快内存
+    ptr_ = nullptr;  // 只是释放所有权，不删除对象，标识unique_ptr不在管理这快内存
     return temp;
 }
 
 template<typename T>
 T& UniquePtr<T>::operator*() const {
+    //解引用操作 直接把内存保管的值送回去
     return *ptr_;
 }
 
